77cd6d2361f7ddd6d5e40ecd90068c372679f0f7
Lisranda <xlisranda@gmail.com>
2019-05-25 00:20:06 -0500

    Animation

---
+++ Assets/Scripts/Mono/Animator_Pawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Animator_Pawn : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = .1f;
+
+    Animator animator;
+    NavMeshAgent agent;
+
+    void Awake () {
+        agent = GetComponent<NavMeshAgent> ();
+        animator = GetComponentInChildren<Animator> ();
+    }
+
+    void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat ("speedPercent" , speedPercent , locomotionAnimationSmoothTime , Time.deltaTime);
+    }
+}
