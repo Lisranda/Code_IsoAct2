4c308a34a06a4da477d39de635ee749c298494ef
Lisranda <xlisranda@gmail.com>
2019-05-31 06:10:43 -0500

    Early Stats
    No where near the plan, messing around with blue print.

---
+++ Assets/Scripts/Class/Character Stats/Statistic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class Statistic
+{
+    [SerializeField] int baseValue;
+    [SerializeField] int currentValue;
+
+    public int GetValue () {
+        return currentValue;
+    }
+}
--- Assets/Scripts/Mono/Pawn.cs
+++ Assets/Scripts/Mono/Pawn.cs
@@ -2,5 +2,19 @@
 
 public class Pawn : MonoBehaviour
 {
-    
+    [Header ("Resource Stats")]
+    [SerializeField] Resource Health;
+    [SerializeField] Resource Mana;
+
+    [Header("Attributes")]
+    [SerializeField] Calculated Strength;
+    [SerializeField] Calculated Dexterity;
+    [SerializeField] Calculated Constitution;
+    [SerializeField] Calculated Wisdom;
+    [SerializeField] Calculated Intelligence;
+
+    [Header ("Calculated Stats")]
+    [SerializeField] Attribute Dodge;
+    [SerializeField] Attribute ManaRegen;
+    [SerializeField] Attribute HitChance;
 }
